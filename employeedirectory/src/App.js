9848aa83a9b6031ba5107430db7caf307db6e347
Hava-hizzle <mikehavrilak@gmail.com>
2020-03-08 08:58:34 -0400

    added src file

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Wrapper from "./components/Wrapper";
+import SearchForm from "./components/SearchForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Wrapper>
+                    <Route exact path="/SearchForm" component={SearchForm} />
+
+                </Wrapper>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
